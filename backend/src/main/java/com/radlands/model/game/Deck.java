3f85bb322695c39bbc6daafc2ebac8a6b61c1f84
henrikts99 <henriko99@live.no>
2026-03-14 21:07:36 +0100

    add deck model class

---
+++ backend/src/main/java/com/radlands/model/game/Deck.java
@@ -0,0 +1,34 @@
+package com.radlands.model.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.radlands.model.card.BaseCard;
+
+public class Deck {
+    List<BaseCard> cards;
+    List<BaseCard> discardPile;
+
+    public Deck(List<BaseCard> cards) {
+        this.cards = new ArrayList<>(cards);
+        shuffle();
+    }
+
+    public BaseCard draw() {
+        if (cards.isEmpty()) {
+            if (discardPile.isEmpty())
+                return null;
+            cards = List.copyOf(discardPile);
+            cards = new ArrayList<>(discardPile);
+            shuffle();
+            discardPile = new ArrayList<>();
+        }
+        return cards.remove(cards.size() - 1);
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+}
